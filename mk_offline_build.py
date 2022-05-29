e4327bb2131d88e20222d0fa9afa85c5581b9300
EvilMask <evilmask.dev@gmail.com>
2022-05-30 03:36:51 +0900

    Add: offline build script

---
+++ mk_offline_build.py
@@ -0,0 +1,17 @@
+import yaml
+import os
+
+with open("mkdocs.yml", mode='r', encoding="utf-8") as fs:
+    data = yaml.load(fs.read(), yaml.SafeLoader)
+    del data['repo_url']
+    del data['site_url']
+    data['plugins'] = data['plugins'][1:]
+    data['use_directory_urls'] = False
+    fs.close()
+    os.rename('mkdocs.yml', 'mkdocs.bak.yml')
+    with open("mkdocs.yml", mode='w+', encoding='utf-8') as ofs:  
+        yaml.dump(data, ofs, encoding='utf-8', allow_unicode=True)
+
+os.system('mkdocs build -d release')
+os.remove('mkdocs.yml')
+os.rename('mkdocs.bak.yml', 'mkdocs.yml')
