c253a63f3ae16d610e9a82f3bc4eef9f9bada5f9
EvilMask <evilmask.dev@gmail.com>
2022-10-08 11:09:19 +0900

    fix: オブ版に要らない`*.map`ファイルも削除するように

--- mk_offline_build.py
+++ mk_offline_build.py
@@ -1,3 +1,4 @@
+import glob
 import yaml
 import os
 
@@ -11,10 +12,12 @@ with open("mkdocs.yml", mode='r', encoding="utf-8") as fs:
     os.rename('mkdocs.yml', 'mkdocs.bak.yml')
     with open("mkdocs.yml", mode='w+', encoding='utf-8') as ofs:  
         yaml.dump(data, ofs, encoding='utf-8', allow_unicode=True)
-
 os.system('mkdocs build -d release')
 os.remove('mkdocs.yml')
 os.rename('mkdocs.bak.yml', 'mkdocs.yml')
+mapFiles = glob.glob("./release/**/*.map", recursive=True)
+for file in mapFiles:
+    os.remove(file)
 os.remove('./release/404.html')
 os.remove('./release/sitemap.xml')
 os.remove('./release/sitemap.xml.gz')
